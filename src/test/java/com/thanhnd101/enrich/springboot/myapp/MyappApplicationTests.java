232cc374b94f85501afa9823fa07838f5491e524
NguyenThanh <71008459+thanhnguyen001@users.noreply.github.com>
2023-05-19 23:11:37 +0700

    week1-1

---
+++ src/test/java/com/thanhnd101/enrich/springboot/myapp/MyappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thanhnd101.enrich.springboot.myapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
