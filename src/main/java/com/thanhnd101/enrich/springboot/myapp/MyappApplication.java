a320f9f95e07e5771d62ef0df9d5fcef03c78a53
NguyenThanh <71008459+thanhnguyen001@users.noreply.github.com>
2023-05-27 10:05:19 +0700

    week2

--- src/main/java/com/thanhnd101/enrich/springboot/myapp/MyappApplication.java
+++ src/main/java/com/thanhnd101/enrich/springboot/myapp/MyappApplication.java
@@ -1,13 +1,14 @@
 package com.thanhnd101.enrich.springboot.myapp;
 
+import com.thanhnd101.enrich.springboot.myapp.dto.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MyappApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MyappApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(MyappApplication.class, args);
+  }
 
 }
--- src/main/java/com/thanhnd101/enrich/springboot/myapp/rest/FunRestController.java
+++ src/main/java/com/thanhnd101/enrich/springboot/myapp/rest/FunRestController.java
@@ -1,5 +1,6 @@
 package com.thanhnd101.enrich.springboot.myapp.rest;
 
+import com.thanhnd101.enrich.springboot.myapp.dto.User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,6 +9,7 @@ public class FunRestController {
 
   @GetMapping("/")
   public String sayHello() {
-    return "Hello World!";
+
+    return "Hello World";
   }
 }
