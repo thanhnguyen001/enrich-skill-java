71cae95b365c8348f28501fa8c1284e83d2f9deb
NguyenThanh <71008459+thanhnguyen001@users.noreply.github.com>
2023-05-31 21:44:07 +0700

    ÆÆweek2-2

--- src/main/java/com/thanhnd101/enrich/springboot/myapp/rest/FunRestController.java
+++ src/main/java/com/thanhnd101/enrich/springboot/myapp/rest/FunRestController.java
@@ -4,6 +4,9 @@ import com.thanhnd101.enrich.springboot.myapp.dto.User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * @author NguyenThanh
+ */
 @RestController
 public class FunRestController {
 
